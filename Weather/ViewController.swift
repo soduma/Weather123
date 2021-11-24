75e34ab08f4b792bedda1eb6de2b93ee8ea9e6ff
soduma <soduma@naver.com>
2021-11-24 15:59:47 +0900

    weather struct, json decoder

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var citiNameLabel: UILabel!
+    @IBOutlet var weatherLabel: UILabel!
+    @IBOutlet var temperatureLabel: UILabel!
+    @IBOutlet var maxLabel: UILabel!
+    @IBOutlet var minLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapPatchButton(_ sender: UIButton) {
+        if let citiName = textField.text {
+            getCurrentWeather(citiName: citiName)
+            view.endEditing(true)
+        }
+    }
+    
+    func getCurrentWeather(citiName: String) {
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(citiName)&appid=0de57519925dcbac571ee1c1b5f7ee4a") else { return }
+        
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            let decoder = JSONDecoder()
+            let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data)
+            debugPrint(weatherInformation)
+        }.resume()
+    }
 }
 
---
+++ Weather/WeatherInformation.swift
@@ -0,0 +1,41 @@
+//
+//  WeatherInformation.swift
+//  Weather
+//
+//  Created by 장기화 on 2021/11/24.
+//
+
+import Foundation
+
+struct WeatherInformation: Codable {
+    let weather: [Weather]
+    let temp: Temp
+    let name: String
+    
+    enum CodingKeys: String, CodingKey {
+        case weather
+        case temp = "main"
+        case name
+    }
+}
+
+struct Weather: Codable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+
+struct Temp: Codable {
+    let temp: Double
+    let feelsLike: Double
+    let tempMin: Double
+    let tempMax: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+    }
+}
